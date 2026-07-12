5596d458ec91b558924c650e4772a4599f4c7fab
RB3572 <86612265+RB3572@users.noreply.github.com>
2026-07-12 18:54:59 -0400

    Load monochrome dashboard enhancements

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './enhancements.css'
+import './dashboard-enhancer.ts'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
