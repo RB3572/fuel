e070f1cf075a06e268758f612b469cc0bd15644e
RB3572 <86612265+RB3572@users.noreply.github.com>
2026-07-15 02:10:31 -0400

    Expose MCP protected resource metadata

---
+++ api/oauth/protected-resource.js
@@ -0,0 +1,11 @@
+import { protectedResourceMetadata } from '../_lib/mcp-auth.js'
+import { methodNotAllowed, sendJson } from '../_lib/http.js'
+
+export default function handler(req, res) {
+  if (req.method !== 'GET') {
+    methodNotAllowed(res, ['GET'])
+    return
+  }
+  res.setHeader('Cache-Control', 'public, max-age=300')
+  sendJson(res, 200, protectedResourceMetadata())
+}
