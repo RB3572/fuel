eabdca00b142206e79975b6663cb3b952ff5b106
RB3572 <86612265+RB3572@users.noreply.github.com>
2026-07-15 02:11:17 -0400

    Preserve OAuth return path through Google sign-in

--- api/auth/google/start.js
+++ api/auth/google/start.js
@@ -1,6 +1,8 @@
 import { randomToken, signState } from '../../_lib/crypto.js'
-import { googleEnv, googleScopes, redirectUri, stateCookieName } from '../../_lib/google.js'
+import { appUrl, googleEnv, googleScopes, redirectUri, stateCookieName } from '../../_lib/google.js'
 import { redirect, serializeCookie } from '../../_lib/http.js'
+
+const returnCookieName = 'fuel_oauth_return'
 
 export default function handler(req, res) {
   if (req.method !== 'GET') {
@@ -21,7 +23,21 @@ export default function handler(req, res) {
   authorizationUrl.searchParams.set('prompt', 'consent')
   authorizationUrl.searchParams.set('state', state)
 
-  redirect(res, authorizationUrl.toString(), [
-    serializeCookie(stateCookieName, signState(state), { maxAge: 10 * 60 }),
-  ])
+  const cookies = [serializeCookie(stateCookieName, signState(state), { maxAge: 10 * 60 })]
+  const returnTo = safeReturnTo(req)
+  if (returnTo !== '/') cookies.push(serializeCookie(returnCookieName, returnTo, { maxAge: 10 * 60 }))
+  redirect(res, authorizationUrl.toString(), cookies)
+}
+
+function safeReturnTo(req) {
+  try {
+    const requestUrl = new URL(req.url, appUrl())
+    const raw = requestUrl.searchParams.get('return_to')
+    if (!raw) return '/'
+    const target = new URL(raw, appUrl())
+    if (target.origin !== new URL(appUrl()).origin || target.pathname.startsWith('//')) return '/'
+    return `${target.pathname}${target.search}`
+  } catch {
+    return '/'
+  }
 }
