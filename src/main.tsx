1c7d6df6fe274ab959b4b2f434a949b1e6d90f10
RB3572 <86612265+RB3572@users.noreply.github.com>
2026-07-15 19:54:36 -0400

    Load Fuel context and calorie chart styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './enhancements.css'
+import './FuelContextEnergy.css'
 import './dashboard-enhancer.ts'
 import App from './App.tsx'
 
